bc3eb136d6be56ecf7387d09ad3761283b06a10b
colenator123-design <meatjuicemeatjuice@gmail.com>
2023-06-17 20:34:51 +0800

    8.34 update

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-tabs bg-color="primary" hide-slider grow>
+  <v-tabs bg-color="black" hide-slider grow>
     <v-spacer />
     <v-tab @click="changeRoute('/')">Home</v-tab>
     <v-tab @click="changeRoute('/rank')">Ranking</v-tab>
     <v-tab @click="changeRoute('/game')">Games</v-tab>
+    <v-img src="/src/assets/images/mlb.jpg" alt="Image"></v-img>
     <v-spacer />
   </v-tabs>
 </template>
--- frontend/src/views/Ranking.vue
+++ frontend/src/views/Ranking.vue
@@ -4,8 +4,11 @@
     <h1>投手排行</h1>
     <Table />
     <br />
-    <h1>打者排行</h1>
+    <h1>打者排行(自責分)</h1>
     <Table_Bat />
+    <br />
+    <h1>打者排行(全壘打)</h1>
+    <Table_Bat_Hr />
     <br />
     <h1>隊伍排行</h1>
     <Table_Team />
@@ -17,12 +20,14 @@
   import Table from '../components/Table.vue'
   import Table_Bat from '../components/Table_bat.vue'
   import Table_Team from'../components/Table_team.vue'
+  import Table_Bat_Hr from'../components/Table_bat_hr.vue'
   export default {
     components: {
       Header,
       Table,
       Table_Bat,
       Table_Team,
+      Table_Bat_Hr,
     },
     // 组件的其他选项和逻辑
   }
