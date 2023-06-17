0d4c20f5e5ea8affe928fa96d742252cc98f3aa8
e54true <e54true@yanjunyingdeMacBook-Pro.local>
2023-06-18 02:23:09 +0800

    renew pitchers.js

--- backend/routes/pitchers.js
+++ backend/routes/pitchers.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const connection = require('../database/connection');
 
 // 取得所有投手資料
-router.get('/', (req, res) => {
-  const query = 'SELECT * FROM pitchers';
+router.get('/byera', (req, res) => {
+  const query = 'SELECT * FROM pitchers ORDER BY era ASC LIMIT 10';
   connection.query(query, (error, results) => {
     if (error) {
       console.error('Error retrieving pitchers:', error);
@@ -15,53 +15,38 @@ router.get('/', (req, res) => {
   });
 });
 
-// 新增投手
-router.post('/', (req, res) => {
-  const { player_id, position, Win, Lose, ERA, WHIP } = req.body;
-  const query = 'INSERT INTO pitchers (player_id, position, Win, Lose, ERA, WHIP) VALUES (?, ?, ?, ?, ?, ?)';
-  connection.query(query, [player_id, position, Win, Lose, ERA, WHIP], (error, result) => {
+router.get('/bywin', (req, res) => {
+  const query = 'SELECT * FROM pitchers ORDER BY win DESC LIMIT 10';
+  connection.query(query, (error, results) => {
     if (error) {
-      console.error('Error adding pitcher:', error);
-      res.status(500).json({ error: 'Failed to add pitcher' });
+      console.error('Error retrieving pitchers:', error);
+      res.status(500).json({ error: 'Failed to retrieve pitchers' });
     } else {
-      res.json({ message: 'Pitcher added successfully', player_id: result.insertId });
+      res.json(results);
     }
   });
 });
 
-// 更新投手資料
-router.put('/:player_id', (req, res) => {
-  const { player_id } = req.params;
-  const { position, Win, Lose, ERA, WHIP } = req.body;
-  const query = 'UPDATE pitchers SET position = ?, Win = ?, Lose = ?, ERA = ?, WHIP = ? WHERE player_id = ?';
-  connection.query(query, [position, Win, Lose, ERA, WHIP, player_id], (error, result) => {
+router.get('/bylose', (req, res) => {
+  const query = 'SELECT * FROM pitchers ORDER BY lose DESC LIMIT 10';
+  connection.query(query, (error, results) => {
     if (error) {
-      console.error('Error updating pitcher:', error);
-      res.status(500).json({ error: 'Failed to update pitcher' });
+      console.error('Error retrieving pitchers:', error);
+      res.status(500).json({ error: 'Failed to retrieve pitchers' });
     } else {
-      if (result.affectedRows === 0) {
-        res.status(404).json({ error: 'Pitcher not found' });
-      } else {
-        res.json({ message: 'Pitcher updated successfully' });
-      }
+      res.json(results);
     }
   });
 });
 
-// 刪除投手
-router.delete('/:player_id', (req, res) => {
-  const { player_id } = req.params;
-  const query = 'DELETE FROM pitchers WHERE player_id = ?';
-  connection.query(query, [player_id], (error, result) => {
+router.get('/bywhip', (req, res) => {
+  const query = 'SELECT * FROM pitchers ORDER BY whip ASC LIMIT 10';
+  connection.query(query, (error, results) => {
     if (error) {
-      console.error('Error deleting pitcher:', error);
-      res.status(500).json({ error: 'Failed to delete pitcher' });
+      console.error('Error retrieving pitchers:', error);
+      res.status(500).json({ error: 'Failed to retrieve pitchers' });
     } else {
-      if (result.affectedRows === 0) {
-        res.status(404).json({ error: 'Pitcher not found' });
-      } else {
-        res.json({ message: 'Pitcher deleted successfully' });
-      }
+      res.json(results);
     }
   });
 });
