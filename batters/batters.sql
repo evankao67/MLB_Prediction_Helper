7dac8e268e87f1ab99239de5ba2be7453fe7b144
kenny3715 <91171195+kenny3715@users.noreply.github.com>
2023-06-18 01:38:39 +0800

    Create batters.sql
    create databases of batters

---
+++ batters/batters.sql
@@ -0,0 +1,23 @@
+CREATE TABLE batters_tmp (
+    playerId VARCHAR(255),
+    `Name` VARCHAR(255),
+    HR VARCHAR(255),
+    `AVG` VARCHAR(255),
+    `OPS` VARCHAR(255)
+);
+
+SHOW VARIABLES LIKE "secure_file_priv";
+SET GLOBAL local_infile = on;
+SHOW VARIABLES LIKE 'local_infile';
+LOAD DATA LOCAL INFILE '/Users/kenny/Desktop/api/batters.csv'
+INTO TABLE batters_tmp
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+INSERT INTO batters(player_id, HR, `AVG`, OPS)
+SELECT playerId, HR, `AVG`, `OPS`
+FROM batters_tmp;
+
+DROP TABLE batters_tmp;
