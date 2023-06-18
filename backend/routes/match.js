a0b33db6572276f6718e19fcc6ff578f0c89db3c
e54true <e54true@yanjunyingdeMacBook-Pro.local>
2023-06-19 01:25:05 +0800

    Upload match.js

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -28,6 +28,29 @@ router.get('/', (req, res) => {
   });
 });
 
+// 取得某日比賽資料
+// 例如 http://localhost:3000/api/match/getByDate?date=2023-06-23
+router.get('/getByDate', (req, res) => {
+  const { date } = req.query;
+  if (!date) {
+    res.status(400).json({ error: 'Missing date parameter' });
+    return;
+  }
+  const query = `SELECT \`time\`, team_home, team_home_winchance, team_home_odds, team_away, team_away_winchance, team_away_odds
+                 FROM rating, \`match\`
+                 WHERE DATE(\`time\`) = ? AND rating.match_id = \`match\`.match_id`;
+  connection.query(query, [date], (error, results) => {
+    if (error) {
+      console.error('Error retrieving matches:', error);
+      res.status(500).json({ error: 'Failed to retrieve matches' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+
+
 
 
 module.exports = router;
