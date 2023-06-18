4528ad9727e779acdff9bf5999511cedfd21621e
ShiveyVer <marklin9133131@gmail.com>
2023-06-18 22:12:49 +0800

    feat: api called when mount homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,17 +3,30 @@
     <Header />
     <Menu_Team />
   </div>
-  
 </template>
 
 <script>
-  import Header from '../components/Header.vue'
-  import Menu_Team from '../components/Menu_team.vue'
-  export default {
-    components: {
-      Header,
-      Menu_Team,
-    },
-  // 组件的其他选项和逻辑
-  }
+import Header from '../components/Header.vue'
+import Menu_Team from '../components/Menu_team.vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+export default {
+  components: {
+    Header,
+    Menu_Team,
+  },
+  setup() {
+    onMounted(() => {
+      console.log('mounted!');
+      axios.get('http://localhost:3001/api/match')
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
+  },
+}
 </script>
