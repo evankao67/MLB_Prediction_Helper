037135cba4b3c9829640ef1614f04788e6e15432
e54true <e54true@yanjunyingdeMacBook-Pro.local>
2023-06-18 19:21:33 +0800

    renew match.js

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require('../database/connection');
 
 // 取得所有比賽資料
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
   const query = 'SELECT * FROM `match`';
   connection.query(query, (error, results) => {
     if (error) {
@@ -14,5 +14,20 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+// 取得當日比賽資料
+router.get('/', (req, res) => {
+  const query = 'SELECT * FROM `match` WHERE DATE(`time`) = CURDATE()';
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error retrieving matches:', error);
+      res.status(500).json({ error: 'Failed to retrieve matches' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+
 
 module.exports = router;
